cf21dad2cfccc420239aba7f0202294780d6d20e
priti maradiya <priti@thebilions.com>
2025-04-25 22:56:35 +0530

    update

--- src/Views/RechargeRecords/RechargeRecordsList.jsx
+++ src/Views/RechargeRecords/RechargeRecordsList.jsx
@@ -800,7 +800,7 @@ const [exportMonth, setExportMonth] = useState(null);
                 textAlign="left"
               />
               <TextField source="remark" label="Remark" />
-              {role === "Player" && 
+              {/* {role === "Player" && 
               <FunctionField
               label="Failed reason"
               render={(record) => {
@@ -809,7 +809,7 @@ const [exportMonth, setExportMonth] = useState(null);
                     return "Payment is not received by TransFi or reverted back to end user due to incorrect information while paying in or compliance checks. This is terminal status"
                  }
               }}
-            />}
+            />} */}
               <FunctionField
                 label="Status"
                 source="status"
