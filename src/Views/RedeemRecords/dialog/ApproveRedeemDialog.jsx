d62b24e47d6a09cb4caf32828b5b09309f3dfb85
priti maradiya <priti@thebilions.com>
2025-11-06 10:49:15 +0530

    Update:Bug Fixes

--- src/Views/RedeemRecords/dialog/ApproveRedeemDialog.jsx
+++ src/Views/RedeemRecords/dialog/ApproveRedeemDialog.jsx
@@ -272,7 +272,7 @@ const ApproveRedeemDialog = ({ open, onClose, record, handleRefresh }) => {
 
                 <p className="mb-1">
                   <small>
-                    Total amount to be redeemed = $
+                    Total amount to be redeemed = 
                     {Math.floor(
                       redeemAmount -
                         (redeemAmount *
