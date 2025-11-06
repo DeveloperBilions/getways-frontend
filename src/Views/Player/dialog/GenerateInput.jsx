e9be51ba1c54868ea5370236d42ee0e65df9b495
priti maradiya <priti@thebilions.com>
2025-11-06 13:02:57 +0530

    Update:bug

--- src/Views/Player/dialog/GenerateInput.jsx
+++ src/Views/Player/dialog/GenerateInput.jsx
@@ -6,8 +6,11 @@ export const generateScInputData = (
   path,
   recipient,
   amountIn,
-  amountOutMinimum
+  amountOutMinimum,
+  deadlineSecFromNow = 900
 ) => {
+  const now = Math.floor(Date.now() / 1000);
+  const deadline = web3.utils.toBN(now + deadlineSecFromNow);
   // Define the exactInput ABI
   const exactInputABI = {
     name: "exactInput",
@@ -19,6 +22,7 @@ export const generateScInputData = (
         components: [
           { type: "bytes", name: "path" },
           { type: "address", name: "recipient" },
+          { type: "uint256", name: "deadline" },
           { type: "uint256", name: "amountIn" },
           { type: "uint256", name: "amountOutMinimum" },
         ],
@@ -31,6 +35,7 @@ export const generateScInputData = (
     {
       path,
       recipient,
+      deadline: deadline.toString(),
       amountIn,
       amountOutMinimum,
     },
