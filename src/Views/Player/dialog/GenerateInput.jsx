6ed4cd300bd5b68bd2209bd755ca88d7045d756b
priti maradiya <priti@thebilions.com>
2025-11-25 19:10:59 +0530

    update:wert

--- src/Views/Player/dialog/GenerateInput.jsx
+++ src/Views/Player/dialog/GenerateInput.jsx
@@ -2,40 +2,66 @@ import Web3 from "web3";
 
 const web3 = new Web3(); // No provider needed for encoding only
 
-export const generateScInputData = (
-  path,
+// export const generateScInputData = (
+//   path,
+//   recipient,
+//   amountIn,
+//   amountOutMinimum
+// ) => {
+//   // Define the exactInput ABI
+//   const exactInputABI = {
+//     name: "exactInput",
+//     type: "function",
+//     inputs: [
+//       {
+//         type: "tuple",
+//         name: "params",
+//         components: [
+//           { type: "bytes", name: "path" },
+//           { type: "address", name: "recipient" },
+//           { type: "uint256", name: "amountIn" },
+//           { type: "uint256", name: "amountOutMinimum" },
+//         ],
+//       },
+//     ],
+//   };
+
+//   // Encode the function call using the exactInput ABI and provided values
+//   const scInputData = web3.eth.abi.encodeFunctionCall(exactInputABI, [
+//     {
+//       path,
+//       recipient,
+//       amountIn,
+//       amountOutMinimum,
+//     },
+//   ]);
+
+//   console.log("Encoded sc_input_data:", scInputData);
+//   return scInputData;
+// };
+
+export const generateScInputData =  ( path,
   recipient,
-  amountIn,
-  amountOutMinimum
-) => {
-  // Define the exactInput ABI
-  const exactInputABI = {
-    name: "exactInput",
-    type: "function",
-    inputs: [
+  amountIn, 
+  amountOutMinimum,   ) => {
+  const rawUsdcAmount = (amountIn * 10**6).toString();
+  const scInputData = web3.eth.abi.encodeFunctionCall({
+    "inputs": [
       {
-        type: "tuple",
-        name: "params",
-        components: [
-          { type: "bytes", name: "path" },
-          { type: "address", name: "recipient" },
-          { type: "uint256", name: "amountIn" },
-          { type: "uint256", name: "amountOutMinimum" },
-        ],
+        "internalType": "address",
+        "name": "recipient",
+        "type": "address"
       },
+      {
+        "internalType": "uint256",
+        "name": "usdcAmount",
+        "type": "uint256"
+      }
     ],
-  };
-
-  // Encode the function call using the exactInput ABI and provided values
-  const scInputData = web3.eth.abi.encodeFunctionCall(exactInputABI, [
-    {
-      path,
-      recipient,
-      amountIn,
-      amountOutMinimum,
-    },
-  ]);
-
-  console.log("Encoded sc_input_data:", scInputData);
-  return scInputData;
-};
+    "name": "buyAOG",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, [recipient, amountIn])
+  return scInputData
+  }
--- src/Views/Player/dialog/RechargeDialog.jsx
+++ src/Views/Player/dialog/RechargeDialog.jsx
@@ -347,13 +347,10 @@ const RechargeDialog = ({ open, onClose, handleRefresh, data }) => {
 
     const user = await currentUser.fetch();
     console.log(user.get("walletAddr"), "userDetails");
-
-    const path =
-      "0x55d398326f99059ff775485246999027b31979550009c4b32d4817908f001c2a53c15bff8c14d8813109be";
-
     const recipient = user.get("walletAddr");
-    const amountIn = (parseFloat(amount) * Math.pow(10, 18)).toString();
+    const amountIn = (parseFloat(amount) * 1e6).toString();
     const amountOutMinimum = "0";
+    const path = "0x9d11ea6e";
 
     const sc_input_data = generateScInputData(
       path,
@@ -365,10 +362,10 @@ const RechargeDialog = ({ open, onClose, handleRefresh, data }) => {
     const signedData = signSmartContractData(
       {
         address: recipient,
-        commodity: "USDT",
+        commodity: "USDC",
         commodity_amount: amount,
-        network: "bsc",
-        sc_address: "0x13f4EA83D0bd40E75C8222255bc855a974568Dd4",
+        network: "ethereum",
+        sc_address:"0x39268dD4b3054A0891A07D7A003C262579Fb5D47",
         sc_input_data: sc_input_data,
       },
       privateKey
